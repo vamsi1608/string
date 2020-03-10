9ea6f5bea027e0a2652051126830698c3021a44a
vamsi1608 <62008479+vamsi1608@users.noreply.github.com>
2020-03-10 14:19:05 +0530

    Add files via upload

---
+++ 2_string.py
@@ -0,0 +1,29 @@
+'''
+1. Using string we can store any value but it should be in single quotes or double quotes.
+2. String is a immutable object
+
+'''
+
+# To define a string variable
+name = 'sri123$#%   '
+
+# If string contains single quote 
+name1 = " I don't know sriram"
+
+# If string contains double quote 
+name2 = ' I know "sriram"'
+
+# If string contains single quote and double quote 
+name3 = ''' I don't know "sriram"'''
+
+
+# To update
+name = 'kumar'
+print(name)
+
+# To Delete 
+del name 
+
+# To Print on console
+print(' Name is : ', name)
+
